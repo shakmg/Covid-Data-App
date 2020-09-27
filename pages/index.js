be216f8b1baefaf02c49ce414fd93650e005c1ca
Shakeel Mohammed <shakeel.mohammed@thetrainline.com>
2020-09-27 20:11:01 +0100

    render global summary info and list of countries - handle request failure

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,91 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import axios from 'axios';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const todaysDate = new Date().toDateString();
+export default class Home extends React.Component {
+  state = {
+    countries: [],
+    globalSummary: {},
+    errorMessage: '',
+  };
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  componentDidMount = () => {
+    axios
+      .get('https://api.covid19api.com/summary')
+      .then((res) => {
+        this.setState({
+          countries: res.data.Countries,
+          globalSummary: res.data.Global,
+        });
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error.message });
+      });
+  };
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Covid Data App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className={styles.HeadingWrapper}>
+          <h1 className={styles.globalTitle}>Global Information Summary</h1>
+          <div className={styles.date}>{todaysDate}</div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        {this.state.errorMessage && (
+          <h3>There has been the following error: {this.state.errorMessage}</h3>
+        )}
+        <div className={styles.globalSummaryWrapper}>
+          <ul className={styles.globalBulletPoints}>
+            <li>
+              New Confirmed Cases Today -{' '}
+              {this.state.globalSummary.NewConfirmed}
+            </li>
+            <li>
+              Total Confirmed Cases - {this.state.globalSummary.TotalConfirmed}
+            </li>
+            <li>
+              New Deaths Today -
+              <span className={styles.warningInformation}>
+                {this.state.globalSummary.NewDeaths}
+              </span>
+            </li>
+            <li>
+              Total Deaths -
+              <span className={styles.warningInformation}>
+                {this.state.globalSummary.TotalDeaths}
+              </span>
+            </li>
+            <li>
+              New Recovered Today -
+              <span className={styles.positiveInformation}>
+                {this.state.globalSummary.NewRecovered}
+              </span>
+            </li>
+            <li>
+              Total Recovered -
+              <span className={styles.positiveInformation}>
+                {this.state.globalSummary.TotalRecovered}
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.countriesWrapper}>
+          <h2 className={styles.countriesTitle}>
+            Countries affected by Covid19
+          </h2>
+          {this.state.countries.map(({ CountryCode, Country }) => {
+            return (
+              <div className={styles.countryName} key={CountryCode}>
+                {Country}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
